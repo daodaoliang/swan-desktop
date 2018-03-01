0145432a1b1bec6d70299e70ec4a1b93c87d9928
Isaac W Hanson <isaac@starlig.ht>
2018-02-28 21:13:58 -0500

    use zenity not matedialog

--- usr/libexec/swan-desktop/swan-changelog.sh
+++ usr/libexec/swan-desktop/swan-changelog.sh
@@ -4,8 +4,8 @@ changelog="/usr/share/doc/swan-desktop/ChangeLog"
 text="See the changelog for important changes"
 if [ ! -e "$timestamp" -o "$timestamp" -ot "$changelog" ]; then
     notify-send --icon dialog-warning "Important Changes!" "$text"
-    matedialog --notification --text "$text"
-    matedialog.exe --width=640 --height=480 --text-info --filename=/usr/share/doc/swan-desktop/ChangeLog --title=ChangeLog
+    zenity --notification --text "$text"
+    zenity --width=640 --height=480 --text-info --filename=/usr/share/doc/swan-desktop/ChangeLog --title=ChangeLog
     touch $timestamp
 fi
 
